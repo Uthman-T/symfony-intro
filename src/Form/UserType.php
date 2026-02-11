b966ef4a2eecead3226b91b3c1a30046862f6e6c
Uthman-T <wakfabrio15@gmail.com>
2026-02-11 16:10:29 +0100

    class relationship

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,17 +4,29 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
-            ->add('password')
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new Length(['min' => 2, 'max' => 12],
+                        minMessage: 'Bon, un peu plus long quand meme',
+                        maxMessage: 'Sheeeesh! Ton pseudo est trop long frerot')
+                ]
+            ])
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('submit', SubmitType::class)
         ;
     }
 
