7bedebe8fcedfd12fd0242e4f9402999fa68a962
Uthman-T <wakfabrio15@gmail.com>
2026-02-13 16:28:56 +0100

    Render products in home page

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function home()
+    public function home(ProductRepository $productRepository): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('home.html.twig', [
+            'products' => $productRepository->findAll()
+        ]);
     }
 }
 
